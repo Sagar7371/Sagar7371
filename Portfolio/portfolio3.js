43442c8aa85cfbfecc5147c700ca6e8e764ce874
Suman Sagar <159580120+Sagar7371@users.noreply.github.com>
2025-02-05 19:02:03 +0530

    Add files via upload

---
+++ Portfolio/portfolio3.js
@@ -0,0 +1,7 @@
+// Add smooth scroll to contact button
+document.getElementById("contactBtn").addEventListener("click", function() {
+    document.getElementById("contact").scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+  
